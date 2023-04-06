43bcbe1e220d3cc3e5081d850a21534cb23d0ad9
Csaba79-coder <csabavadasz79@gmail.com>
2023-04-06 12:39:51 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/sport/controller/FootballController.java
@@ -0,0 +1,34 @@
+package com.csaba79coder.sport.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping
+public class FootballController {
+
+    @Value("${spring.sportmonks.apikey}")
+    private String apiKey;
+
+    @GetMapping("/football/teams")
+    public ResponseEntity<Object> renderAllFootballTeams() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", apiKey);
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        ResponseEntity<Object> response = restTemplate.exchange(
+                "https://api.sportmonks.com/v3/football/teams",
+                HttpMethod.GET,
+                entity,
+                Object.class
+        );
+        return response;
+    }
+}
